b0eb3125d901a9cec7ec3ed75eb687e4ddd18ef3
liuhai <1242970175@qq.com>
2021-01-14 15:01:52 +0800

    shop 实体类修改

--- src/main/java/com/jk/pojo/ShopBean.java
+++ src/main/java/com/jk/pojo/ShopBean.java
@@ -1,6 +1,11 @@
 package com.jk.pojo;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 /**
  * @program: houtai
@@ -9,6 +14,7 @@ import lombok.Data;
  * @create: 2021-01-12 16:41
  */
 @Data
+@Document(indexName = "shopping",type = "2007a")
 public class ShopBean {
   /*  sname	String	商品名称
     stypeid	int	商品类型
@@ -20,18 +26,29 @@ public class ShopBean {
     scount	Int	库存
     ordernum	Int	购买量
     status	Int	商品状态*/
+    @Id
     private Integer id;
+  @Field(type = FieldType.Text,analyzer = "ik_max_word")
     private String sname;
+  @Field(type = FieldType.Keyword)
     private Integer stypeid;
+  @Field(type = FieldType.Keyword)
     private Double sprice;
+  @Field(type = FieldType.Text,analyzer = "ik_max_word")
     private String sinfo;
+  @Field(type = FieldType.Keyword)
     private String sdate;
+  @Field(type = FieldType.Keyword)
     private Integer sbaodate;
+  @Field(type = FieldType.Keyword)
     private String simage;
+  @Field(type = FieldType.Keyword)
     private Integer scount;
+  @Field(type = FieldType.Keyword)
     private Integer ordernum;
+ @Transient
     private Integer status;
-
+  @Field(type = FieldType.Text,analyzer = "ik_max_word")
     private String stypename;//类型
 
 
